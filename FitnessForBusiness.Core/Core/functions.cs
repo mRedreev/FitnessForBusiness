9ace0bb3e934678b4d1d1fcc9fa11076e3b36d39
Юлия Гильмуллина <gilmullinau@gmail.com>
2022-05-17 15:23:29 +0300

    nameofgoal

---
+++ FitnessForBusiness.Core/Core/functions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessForBusiness.Core.Core
+{
+    public class functions
+    {
+        public static string NameOfLevel(bool? level)
+        {
+            if (level == null) return "Beginner";
+            else if (level == false) return "Amateur";
+            else if (level == true) return "Advanced";
+            return null;
+        }
+    }
+}
