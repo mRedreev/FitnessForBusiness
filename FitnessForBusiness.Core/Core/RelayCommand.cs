8d1107c48807c3a5e08bb3e2967f8ced8d89ef65
Михаил Редреев <moredreev@edu.hse.ru>
2022-05-18 21:24:13 +0300

    12

--- FitnessForBusiness.Core/Core/RelayCommand.cs
+++ FitnessForBusiness.Core/Core/RelayCommand.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace DesignBomba.Core
+namespace FitnessForBusiness.Core.Core
 {
     internal class RelayCommand : ICommand
     {
