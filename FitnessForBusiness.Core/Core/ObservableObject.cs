a134b83b575d0aca9146a9fd9250fe2e472a725d
Михаил Редреев <moredreev@edu.hse.ru>
2022-06-15 17:37:51 +0300

    c

--- FitnessForBusiness.Core/Core/ObservableObject.cs
+++ FitnessForBusiness.Core/Core/ObservableObject.cs
@@ -4,13 +4,13 @@ using System.Runtime.CompilerServices;
 
 namespace FitnessForBusiness.Core.Core
 {
-    public class ObservableObject : INotifyPropertyChanged
-    {
-        public event PropertyChangedEventHandler PropertyChanged;
+    //public class ObservableObject : INotifyPropertyChanged
+    //{
+    //    public event PropertyChangedEventHandler PropertyChanged;
 
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
-    }
+    //    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    //    {
+    //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    //    }
+    //}
 }
