75a2a1d4fea0d10e585947987340f5385799c573
Полина Абрамкина <ppabramkina@edu.hse.ru>
2022-06-17 16:04:23 +0300

    show train atributes

--- FitnessForBusiness.Design/CurrentTrainingWindow.xaml.cs
+++ FitnessForBusiness.Design/CurrentTrainingWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FitnessForBusiness.Core.Models;
+using FitnessForBusiness.Core.Storages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,32 +21,42 @@ namespace FitnessForBusiness.Design
     /// </summary>
     public partial class CurrentTrainingWindow : Window
     {
+        Training _training;
+
         public CurrentTrainingWindow()
         {
+            IStorage _storage = new Context();
+            _training = _storage.GetTrainings.First();
             InitializeComponent();
+
+            ExcercizesListBox.ItemsSource = _training.Excercises;
         }
 
        
         private void TrainingNameTextBox_Initialized(object sender, EventArgs e)
         {
-
+            var NameTextBlock = sender as TextBlock;
+            NameTextBlock.Text = _training.Name;
         }
 
-
-
-        private void TrainingGif_Initialized(object sender, EventArgs e)
+        private void ExcercizeGif_Initialized(object sender, EventArgs e)
         {
-
+            var ExcercizeGifMediaElement = sender as MediaElement;
+            var excercize = ExcercizeGifMediaElement.DataContext as Excercise;
+            ExcercizeGifMediaElement.Source = new Uri(excercize.VideoSource);
         }
 
         private void ExcerciseNameTextBlock_Initialized(object sender, EventArgs e)
         {
-
+            var ExcercizeNameTextBlock = sender as TextBlock;
+            var excercize = ExcercizeNameTextBlock.DataContext as Excercise;
+            ExcercizeNameTextBlock.Text = excercize.Name;
         }
 
         private void LengthAndBreakTextBlock_Initialized(object sender, EventArgs e)
         {
-
+            var LengthAndBreakTextBlock = sender as TextBlock;
+            LengthAndBreakTextBlock.Text = $"Длительность упражнения: {_training.ExcerciseLength.ToString()}, перерыв: {_training.BreakLength.ToString()}";
         }
     }
 }
