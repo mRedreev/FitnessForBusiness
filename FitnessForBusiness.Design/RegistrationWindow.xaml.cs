5ce60ad90cb83e60474bbc481cdec85c4e7343c4
Юлия Гильмуллина <gilmullinau@gmail.com>
2022-06-17 13:23:46 +0300

    comboboxes are not empty in registration

--- FitnessForBusiness.Design/RegistrationWindow.xaml.cs
+++ FitnessForBusiness.Design/RegistrationWindow.xaml.cs
@@ -54,6 +54,9 @@ namespace FitnessForBusiness.Design
             bool userNotExists = false;
             var listTextBoxes = new List<TextBox>(){NameBox, SurnameTextBox, EmailTextBox};
             bool ifBoxesNotEmpty = functions.TextsBoxIsNotEmpty(listTextBoxes);
+            ifBoxesNotEmpty = functions.ComboBoxIsNotEmpty(GoalComboBox, "Your Main Goal");
+            ifBoxesNotEmpty = functions.ComboBoxIsNotEmpty(LeveloComboBox, "Your Level");
+            ifBoxesNotEmpty = functions.ComboBoxIsNotEmpty(AvatarComboBox, "");
             if (ifBoxesNotEmpty)
             {
                 if (PasswordBox.Password == PasswordAgianTextBox.Password && BitrhDateDatePicker.SelectedDate.ToString() != "")
