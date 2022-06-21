0445303a1bd618d3b97566e969eb2d16dd83e839
Михаил Редреев <moredreev@edu.hse.ru>
2022-06-21 19:29:01 +0300

    completedprofileview

--- FitnessForBusiness.Design/MVVM/View/ProfileView.xaml.cs
+++ FitnessForBusiness.Design/MVVM/View/ProfileView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using FitnessForBusiness.Core.Models;
+using FitnessForBusiness.Core.Storages;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,27 +29,31 @@ namespace FitnessForBusiness.Design.MVVM.View
 
         private void Image_Initialized(object sender, EventArgs e)
         {
-
+            //тут должен быть аватар профиля текущего пользователя
         }
 
         private void NameTextBox_Initialized(object sender, EventArgs e)
         {
-
-        }
-
-        private void LevelTextBox_Initialized(object sender, EventArgs e)
-        {
-
+            //имя текущего пользователя
         }
 
         private void SurnameTextBox_Initialized(object sender, EventArgs e)
         {
-
+            //фамилия текущего пользователя
+        }
+        private void LevelTextBox_Initialized(object sender, EventArgs e)
+        {
+            //уровень текущего пользователя
         }
 
         private void GoalComboBox_Initialized(object sender, EventArgs e)
         {
+            //цель текущего пользователя
+        }
 
+        private void SaveChangesButton_Click(object sender, RoutedEventArgs e)
+        {
+            //применить изменения всего, что было отредактировано
         }
     }
 }
