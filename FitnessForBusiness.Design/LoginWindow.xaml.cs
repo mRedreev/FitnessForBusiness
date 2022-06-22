8c43fd07ccfdfb02d7de6387f682926568079753
Юлия Гильмуллина <gilmullinau@gmail.com>
2022-06-22 19:28:17 +0300

    _storage

--- FitnessForBusiness.Design/LoginWindow.xaml.cs
+++ FitnessForBusiness.Design/LoginWindow.xaml.cs
@@ -41,7 +41,7 @@ namespace FitnessForBusiness.Design
                     else if (user.Password != LogInPasswordBox.Password) MessageBox.Show("Wrong password");
                     else
                     {
-                        TrainingCatalog trainingCatalog = new TrainingCatalog(user);
+                        TrainingCatalog trainingCatalog = new TrainingCatalog(user, _storage);
                         trainingCatalog.Show();
                         this.Close();
                     }
